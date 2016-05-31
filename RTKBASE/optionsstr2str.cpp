4a410ec5b214b28c7b1bf26a2313057323295b28
Francklin2 <francklin2@wanadoo.fr>
2016-05-31 03:09:45 +0200

    New file for the ui but still not working, the file list is now correct but i cant get string from QlineEdit. The manual mode only create a sauvegardebasepositionManual.txt file

---
+++ RTKBASE/optionsstr2str.cpp
@@ -0,0 +1,134 @@
+#include "optionsstr2str.h"
+#include "ui_optionsstr2str.h"
+#include "affichestr2str.h"
+
+#include <QTextStream>
+#include <QFile>
+
+int i;
+char LongManual;
+char LongManualLine;
+char LatManual;
+char AltiManual;
+//char LatManualLine;
+//char CoordManual;
+
+OptionsStr2str::OptionsStr2str(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::OptionsStr2str)
+{
+    ui->setupUi(this);
+    QObject::connect(ui->pushButtonDefault1,SIGNAL(clicked()),this,SLOT(OuvreDefault1()));
+    QObject::connect(ui->pushButtonDefault2,SIGNAL(clicked()),this,SLOT(OuvreDefault2()));
+    //QLineEdit *LongManualLine = new QLineEdit("0");
+
+}
+
+OptionsStr2str::~OptionsStr2str()
+{
+    delete ui;
+}
+
+void OptionsStr2str::OuvreDefault1()
+{
+    arga={"carlepremierargtoujorsleprog","-in","serial://ttyACM0:115200:8:n:1:#ubx","-msg","1004,1019,1012,1020,1006,1008"};
+
+
+    AfficheStr2str affichesolutionsStr2str(arga);
+    this->hide();
+    affichesolutionsStr2str.exec();
+}
+
+void OptionsStr2str::OuvreDefault2()
+{
+    //arga={"carlepremierargtoujorsleprog","-in","serial://ttyACM0:115200:8:n:1:#ubx","-out","serial://ttyUSB0:38400:8:n:1:#RTCM3","-msg","1004,1019,1012,1020,1006,1008"};
+    arga={"carlepremierargtoujorsleprog","-in","serial://ttyACM0:115200:8:n:1:#ubx","-out","serial://ttyUSB0:38400:8:n:1:#RTCM3","-msg","1004,1019,1012,1020,1006,1008","-p","48.2","12.2","120.23"};
+
+    // crash arga={"carlepremierargtoujorsleprog","-in","serial://ttyACM0:115200:8:n:1:#ubx","-out","serial://ttyUSB0:38400:8:n:1:#RTCM3","-msg","1004,1019,1012,1020,1006,1008", "-p","48,12,120"};
+    // crash arga={"carlepremierargtoujorsleprog","-in","serial://ttyACM0:115200:8:n:1:#ubx","-out","serial://ttyUSB0:38400:8:n:1:#RTCM3","-msg","1004,1019,1012,1020,1006,1008", "-p","48 12 120"};
+    /* essayer avec:
+    arga={"carlepremierargtoujorsleprog","-in","serial://ttyACM0:115200:8:n:1:#ubx","-out","serial://ttyUSB0:38400:8:n:1:#RTCM3","-msg","1004,1019,1012,1020,1006,1008", "-p","48", " 12", " 120"};*/
+
+    AfficheStr2str affichesolutionsStr2str(arga);
+    this->hide();
+    affichesolutionsStr2str.exec();
+}
+
+
+
+
+void OptionsStr2str::on_LongManualLine_editingFinished()
+{
+
+  //  QString LongManual;
+    QLineEdit *LongManualLine = new QLineEdit( this );
+    QString LongManual = ui->LongManualLine->text();
+    //QLabel* label = new QLabel( LongManual, this);
+
+
+}
+
+
+void OptionsStr2str::on_LatManualLine_editingFinished()
+{
+    QString LatManual = ui->LatManualLine->text();
+}
+
+void OptionsStr2str::on_AltiManualLine_editingFinished()
+{
+
+}
+
+
+void OptionsStr2str::on_radioButtonPositionManual_toggled(bool checked)
+{
+
+    /*Open, read line by line and then close file*/
+    int i=1;
+    QStringList list;
+    QString fileName = "sauvegardepourbaseManual.txt";
+    QFile fichier(fileName);
+    fichier.open(QIODevice::ReadOnly | QIODevice::Text);
+    //---------verifier ouverture fichier......
+    QTextStream flux(&fichier);
+    QString ligne;
+    while(! flux.atEnd())
+    {
+        ligne = flux.readLine();
+        //traitement de la ligne
+        qDebug()<<ligne;
+        list<<ligne;
+        i=i+1;
+    }
+    fichier.close();
+
+
+    /*Save Manual position. This position will be used in Manual Base Mode*/
+
+    QFile CoordManual("sauvegardepourbaseManual.txt");
+
+    if (!CoordManual.open(QIODevice::WriteOnly | QIODevice::Text))
+        return;
+
+   // QTextStream filestream(&CoordManual);
+
+
+
+
+    QTextStream out1(&CoordManual);
+      if(i>1)
+    {
+        out1<<list[0]<<endl;
+        out1<<list[1]<<endl;
+        out1<<"1255,555"<<endl;
+    }
+
+    //  out1 << &CoordManual;
+
+
+    CoordManual.close();
+
+}
+
+
+
