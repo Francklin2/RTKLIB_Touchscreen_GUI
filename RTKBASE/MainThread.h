8c0ef9e5c118e2db136004abed16d9fc59b38a15
Francklin2 <francklin2@wanadoo.fr>
2016-06-09 01:43:46 +0200

    Add GNU GPL License

--- RTKBASE/MainThread.h
+++ RTKBASE/MainThread.h
@@ -1,3 +1,26 @@
+// RTKBASE is a GUI interface for RTKLIB made for the Raspberry pî and a touchscreen  
+//   Copyright (C) 2016  
+//   David ENSG PPMD-15  
+//   Francklin N'guyen van <francklin2@wanadoo.fr>
+//   Sylvain Poulain <sylvain.poulain@giscan.com>
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+
+
 #ifndef MAINTHREAD_H
 #define MAINTHREAD_H
 #include <QtCore>
