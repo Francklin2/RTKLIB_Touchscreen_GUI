558cdef59e8f0910d68f1040181520c090cfcbc7
Francklin2 <francklin2@wanadoo.fr>
2019-03-27 22:21:23 +0100

    Update the install scripts, now working on Raspberry pi and x86 desktop PC

--- Installer/2-Build_rtkbase.sh
+++ Installer/2-Build_rtkbase.sh
@@ -1,8 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 # Build RTKBASE 
-cd /home/pi/RTKLIB_Touchscreen_GUI/RTKBASE
+cd $HOME/RTKLIB_Touchscreen_GUI/RTKBASE
 # restore rights for build script
 chmod +x build_rtkbase_Qt5_ARM.sh
 chmod +x build_rtkbase_Qt5_x86.sh
-./build_rtkbase_Qt5_ARM.sh
+
+
+PS3='Please enter your choice:'
+options=("Build on ARM" "Build on x86" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Build on ARM")
+            echo "you choose ARM build"
+            ./build_rtkbase_Qt5_ARM.sh
+            break
+            ;;
+        "Build on x86")
+            echo "you choose x86 build"
+            ./build_rtkbase_Qt5_x86.sh
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
