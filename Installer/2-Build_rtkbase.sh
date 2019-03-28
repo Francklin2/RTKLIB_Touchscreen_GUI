80dcfe5f5626dc3a9d45f09702ed1819423170f0
Francklin2 <francklin2@wanadoo.fr>
2019-03-28 23:45:42 +0100

    Updating build script with platform detection, add a build script for x86_32 and rename x86 to x86_64

--- Installer/2-Build_rtkbase.sh
+++ Installer/2-Build_rtkbase.sh
@@ -4,27 +4,17 @@
 cd $HOME/RTKLIB_Touchscreen_GUI/RTKBASE
 # restore rights for build script
 chmod +x build_rtkbase_Qt5_ARM.sh
-chmod +x build_rtkbase_Qt5_x86.sh
+chmod +x build_rtkbase_Qt5_x86_64.sh
+chmod +x build_rtkbase_Qt5_x86_32.sh
 
-
-PS3='Please enter your choice:'
-options=("Build on ARM" "Build on x86" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Build on ARM")
-            echo "you choose ARM build"
-            ./build_rtkbase_Qt5_ARM.sh
-            break
-            ;;
-        "Build on x86")
-            echo "you choose x86 build"
-            ./build_rtkbase_Qt5_x86.sh
-            break
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
+            echo "Starting RTKbase build"
+	    cd $HOME/RTKLIB_Touchscreen_GUI/RTKBASE
+            # Find current arch
+	    UNAME=$(uname -m)
+	    if [[ "$UNAME" == *"x86_64"* ]] ; then
+	    echo "Build for Linux 64 bits" | ./build_rtkbase_Qt5_${UNAME}.sh
+	    elif [[ "$UNAME" == *"arm"* ]] ; then
+	    echo "Build for ARM | ./build_rtkbase_Qt5_ARM.sh"
+	    elif [[ "$UNAME" == *"i"* ]] ; then
+	    echo "Build for Linux 32 bits" | ./build_rtkbase_Qt5_x86_32.sh
+	    fi
