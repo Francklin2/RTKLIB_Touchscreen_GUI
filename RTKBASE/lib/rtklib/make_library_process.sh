d9463138e7738da237a0bea7de907607158c48b9
Francklin2 <francklin2@wanadoo.fr>
2019-04-07 01:52:47 +0200

    Updating post process build script and lib folder for 2.43b31 ,rnx2rtkpwas was not building due to missing lib/iers folder

--- RTKBASE/lib/rtklib/make_library_process.sh
+++ RTKBASE/lib/rtklib/make_library_process.sh
@@ -4,8 +4,12 @@ cd ./app/convbin/gcc
 make clean
 make -j2
 sudo make install
-cd ../../
-cd rnx2rtkp/gcc
+cd ../../../
+cd lib/iers/gcc
+make clean
+make -j2
+cd ../../../
+cd ./app/rnx2rtkp/gcc
 make clean
 make -j2
 sudo make install
